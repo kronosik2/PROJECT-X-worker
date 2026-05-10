d52d1827fd7c56f4950a0fecd5e5901f0020409e
kronosik2 <kronosik2@gmail.com>
2026-05-10 21:50:13 +0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = 'https://pnnyybkffogjjnftrgpk.supabase.co:6543';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
