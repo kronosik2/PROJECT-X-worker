3abdf8c321efc0273453f708dfbce3440f596d73
kronosik2 <kronosik2@gmail.com>
2026-05-10 19:10:15 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
   basePath: '/PROJECT-X-worker',
   assetPrefix: '/PROJECT-X-worker',
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 export default nextConfig;
