18aa34d64680b548cc46d3ee10a8be351ddbeee9
kronosik2 <kronosik2@gmail.com>
2026-05-10 18:32:42 +0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/PROJECT-X-worker',
-  assetPrefix: '/PROJECT-X-worker',
-};
-
-export default nextConfig;
